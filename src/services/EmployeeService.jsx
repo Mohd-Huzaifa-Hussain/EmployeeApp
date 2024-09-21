f52f95d4684b9fcb7795de08297ae44e86b300fe
Mohd-Huzaifa-Hussain <121728623+Mohd-Huzaifa-Hussain@users.noreply.github.com>
2024-09-21 20:58:49 +0530

    Update lcoal EmployeeService.jsx

--- src/services/EmployeeService.jsx
+++ src/services/EmployeeService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://emprm.onrender.com/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
 
 export const getEmployees = () => {
   return axios.get(EMPLOYEE_API_BASE_URL);
